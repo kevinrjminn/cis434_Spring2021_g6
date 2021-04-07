c423656d76289bb48869dbefc72ebb4f380c248e
Kevin <kevin.rj.minn@outlook.com>
2021-04-07 00:21:38 -0400

    add classes and id

--- TicTacToe/src/com/kevinminn/Cis434/Controller.java
+++ TicTacToe/src/com/kevinminn/Cis434/Controller.java
@@ -1,4 +1,27 @@
 package com.kevinminn.Cis434;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+
 public class Controller {
+    @FXML
+    private RadioButton select;
+
+
+    @FXML
+    private Button startButton;
+
+    @FXML
+    private TextField nameField;
+
+    @FXML
+    private Button quitButton;
+
+
+    @FXML
+    private Button helloButton;
+
+
 }
