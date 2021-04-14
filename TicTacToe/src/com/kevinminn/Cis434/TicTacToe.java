1222f89493378e1112fed3716821da78802d3ec7
shysept <milli123@gmail.com>
2021-04-14 15:16:19 -0400

    added createboardgui, button action and exit button

--- TicTacToe/src/com/kevinminn/Cis434/TicTacToe.java
+++ TicTacToe/src/com/kevinminn/Cis434/TicTacToe.java
@@ -39,19 +39,92 @@ public class TicTacToe extends JFrame {
     }
 //aaaaaa more test
     //Implement the method to creat GUI board
-    private void createBoardGUI()
-    {
-        //Set the title of the board
-        title = new JLabel("TIC TAC TOE Game!");
-        titlePanel = new JPanel();
-        //set the font type and size
-        title.setFont(new Font(Font.MONOSPACED, 0, 40));
-        //set the fond color
-        title.setForeground(Color.BLUE);
-        //add it to the panel
-        titlePanel.add(title);
+private void createBoardGUI()
+{
+    //Set the title of the board
+    title = new JLabel("TIC TAC TOE Game!");
+    titlePanel = new JPanel();
+    //set the font type and size
+    title.setFont(new Font(Font.MONOSPACED, 0, 40));
+    //set the fond color
+    title.setForeground(Color.BLUE);
+    //add it to the panel
+    titlePanel.add(title);
 
-        //Create panel
-        boardPanel = new JPanel();
-        boardPanel.setLayout(LAYOUT);
+    //Create panel
+    boardPanel = new JPanel();
+    boardPanel.setLayout(LAYOUT);
+
+    //Implement button action
+    class ButtonListener implements ActionListener
+    {
+        @Override
+        public void actionPerformed(ActionEvent ae)
+        {
+            //increment turn for each selection
+            turns++;
+
+            //put the Mark alteranatively
+            if(turns % 2 == 0)
+            {
+                MARK = "O";
+            }
+            else
+            {
+                MARK = "X";
+            }
+            //create button
+            JButton btn = (JButton)ae.getSource();
+            btn.setForeground(Color.YELLOW);
+            btn.setText(MARK);
+            btn.setEnabled(false);
+            displayWinner();
+        }
+    }
+    //Create object for the ButtonListener
+    ActionListener buttonListener = new ButtonListener();
+    //use for-loop add buttons in the panel
+    for(int i=0; i<9; i++)
+    {
+        buttons[i] = new JButton("");
+        buttons[i].setBackground(Color.BLACK);
+        buttons[i].setForeground(Color.YELLOW);
+        buttons[i].setFont(new Font(Font.SERIF, 0, 24));
+        buttons[i].addActionListener(buttonListener);
+        boardPanel.add(buttons[i]);
+    }
+
+    //Create EXit button
+    exit = new JButton("EXIT");
+    exit.setFont(new Font(Font.MONOSPACED, 0, 24));
+    exit.setForeground(Color.RED);
+    //set the action listenet for exit button
+    class QuitListener implements ActionListener
+    {
+        @Override
+        public void actionPerformed(ActionEvent ae)
+        {
+            System.exit(0);
+        }
+    }
+    ActionListener quitListener = new QuitListener();
+    exit.addActionListener(quitListener);
+
+    //Add the panel
+    wholePanel = new JPanel();
+    wholePanel.setLayout(new BorderLayout());
+    wholePanel.add(titlePanel, BorderLayout.NORTH);
+    wholePanel.add(boardPanel, BorderLayout.CENTER);
+    wholePanel.add(exit, BorderLayout.SOUTH);
+    add(wholePanel);
 }
+
+    //Implement the method to check for the horizontal wins
+    public void checkHorzWins()
+    {
+        if (buttons[0].getText().equals(buttons[1].getText())
+                && buttons[1].getText().equals(buttons[2].getText())
+                && buttons[0].getText().equals("")==false)
+        {
+            win=true;
+        }
